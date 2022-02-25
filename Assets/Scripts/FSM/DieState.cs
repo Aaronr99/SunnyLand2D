a0e86a9d4a2daedfaf499e1bd9b087b1eab9db77
Aaronr99 <38481944+Aaronr99@users.noreply.github.com>
2022-02-25 14:31:24 -0300

    DieStateFix
    No se ejecutaba una animacion por codigo y no como no aportaba ya que la camara se centra sola la saque

--- Assets/Scripts/FSM/DieState.cs
+++ Assets/Scripts/FSM/DieState.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 public class DieState : State
 {
     private bool assigned;
-    private Vector3 screenCenter;
-    private float timer;
     public DieState(PlayerController pPlayerController) : base(pPlayerController)
     {
     }
@@ -14,9 +12,6 @@ public class DieState : State
     {
         if (!assigned)
         {
-            Camera cam = Camera.main;
-            // Se guarda el centro de la pantalla
-            screenCenter = cam.ViewportToWorldPoint(new Vector3(0.5f, 0.55f, cam.nearClipPlane));
             // Ejecuta una animacion de muerte
             iPC.anim.SetTrigger("die");
             iPC.boxCollider.enabled = false;
@@ -24,17 +19,6 @@ public class DieState : State
             iPC.rBody.gravityScale = 0;
             assigned = true;
             iPC.DieSound();
-        }
-        // Espera un segundo para que ejecute la animacion
-        timer += Time.deltaTime;
-        if( timer > 1f)
-        {
-            // Si todavia no esta cerca del centro de la pantalla
-            if (Vector3.Distance(iPC.transform.position, screenCenter) < 0.01f)
-            {
-                // Mover al jugador al centro de la pantalla
-                iPC.transform.position = Vector3.MoveTowards(iPC.transform.position, screenCenter, 4 * Time.deltaTime);
-            }
-        }    
+        }   
     }
 }
